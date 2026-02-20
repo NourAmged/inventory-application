273d720c716445715e960730e06be4831979c585
Nour Amged <nouramgged2005@gmail.com>
2026-02-21 01:15:19 +0200

    fixed small problem in database

--- db/populatedb.js
+++ db/populatedb.js
@@ -7,17 +7,17 @@ CREATE TABLE IF NOT EXISTS products (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR ( 33 ),
     category VARCHAR (15),
-    price DECIMAL(5, 2) 
+    price DECIMAL(5, 2), 
     quantity INTEGER,
     description VARCHAR(255),
-    src TEXT DEFAULT '../images/fork-and-knife-with-plate-svgrepo-com.svg' 
+    image TEXT DEFAULT '../images/fork-and-knife-with-plate-svgrepo-com.svg' 
 );
 
 INSERT INTO products (name, category, price, quantity, description, image)
 VALUES
     ('Bread', 'Bakery', 1.50, 50, 'Freshly baked bread', '../images/bread-svgrepo-com.svg'),
-    ('Cheese', 'Dairy', 1.99, 20, 'Delicious cheddar cheese, perfect for sandwiches or melting', '../images/Delicious cheddar cheese, perfect for sandwiches or melting.svg'),
-    ('Apple', 'Fruits', 0.99, 100, 'A crisp and juicy red apple, perfect for snacking or adding to salads.', '../images/apple-svgrepo-com.svg'),
+    ('Cheese', 'Dairy', 1.99, 20, 'Delicious cheddar cheese, perfect for sandwiches or melting', '../images/cheese-svgrepo-com.svg'),
+    ('Apple', 'Fruits', 0.99, 100, 'A crisp and juicy red apple, perfect for snacking or adding to salads.', '../images/apple-svgrepo-com.svg');
 `;
 
 async function main() {
