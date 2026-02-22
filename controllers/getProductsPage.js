a140dab810c0bb0439589294333ab577e95d90a7
Nour Amged <nouramgged2005@gmail.com>
2026-02-22 03:29:50 +0200

    improved a solution for coloring category

--- controllers/getProductsPage.js
+++ controllers/getProductsPage.js
@@ -1,8 +1,16 @@
 const { getProducts } = require("../db/queries");
 
+
 async function getProductsPage(req, res) {
+    const categoryColor = {
+        Bakery: '#ffc26c',
+        Fruits: '#FF0062',
+        Dairy: '#FFFEEB',
+    }
+
+
     const products = await getProducts();
-    res.render("productsPage", { products: products })
+    res.render("productsPage", { products: products, categoryColor: categoryColor })
 }
 
 module.exports = getProductsPage;
