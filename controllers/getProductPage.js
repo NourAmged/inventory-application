87e5f94494db09f43658f0dde25055730b4084cd
Nour Amged <nouramgged2005@gmail.com>
2026-02-27 23:12:02 +0200

    added product page

--- controllers/getProductPage.js
+++ controllers/getProductPage.js
@@ -3,7 +3,6 @@ const { categoryColor } = require("../categoryColor");
 
 async function getProductPage(req, res) {
     const { id } = req.params;
-    console.log(id);
 
     const [product] = await getProduct(id);
     res.render("productPage", { product: product, categoryColor: categoryColor[product.category] });
